9589a6b68c7acacdb89a5c7b961d727ab94ac866
CrabeNeo <133020227+CrabeNeo@users.noreply.github.com>
2026-06-04 16:03:56 +0200

    Fix formatting of tvaRate in InvoiceRequestDto

--- src/main/java/fr/iut/rodez/hotel/infrastructure/api/dto/InvoiceRequestDto.java
+++ src/main/java/fr/iut/rodez/hotel/infrastructure/api/dto/InvoiceRequestDto.java
@@ -2,5 +2,5 @@ package fr.iut.rodez.hotel.infrastructure.api.dto;
 
 public record InvoiceRequestDto(
         Long bookingId,
-        Double tvaRate // Ou BigDecimal selon ton modèle domain
+        Double tvaRate
 ) {}
