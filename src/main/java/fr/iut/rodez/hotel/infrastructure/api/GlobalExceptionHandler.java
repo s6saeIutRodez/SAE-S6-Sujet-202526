4756abf13e8a229ddc1cff466326f285bf3cea93
CrabeNeo <133020227+CrabeNeo@users.noreply.github.com>
2026-06-04 16:05:12 +0200

    Update GlobalExceptionHandler.java

--- src/main/java/fr/iut/rodez/hotel/infrastructure/api/GlobalExceptionHandler.java
+++ src/main/java/fr/iut/rodez/hotel/infrastructure/api/GlobalExceptionHandler.java
@@ -11,14 +11,13 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    // Capture les exceptions levées par ton Domaine (Doublons, Statuts invalides...)
     @ExceptionHandler({IllegalStateException.class, IllegalArgumentException.class})
     public ResponseEntity<Map<String, Object>> handleBusinessExceptions(RuntimeException ex) {
         Map<String, Object> body = new HashMap<>();
         body.put("timestamp", LocalDateTime.now());
         body.put("status", HttpStatus.BAD_REQUEST.value());
         body.put("error", "Bad Request / Règle Métier Violée");
-        body.put("message", ex.getMessage()); // <-- Affiche ton message personnalisé
+        body.put("message", ex.getMessage());
         body.put("path", "/invoices");
 
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
